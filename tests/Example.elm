6f60ad3df53e24eda8ae31c4f046be49732d13f5
515hikaru <12kojima.takahiro@gmail.com>
2019-04-11 11:33:21 +0900

    Add a simple test

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,10 +1,16 @@
-module Example exposing (..)
+module Example exposing (suite)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Main exposing (twitter)
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "foo"
+        [ test "bar" <|
+            \_ ->
+                twitter.mediaName
+                    |> Expect.equal "Twitter"
+        ]
